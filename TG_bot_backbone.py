c43a4ad373b10705a12e2bb78b53be8f326e8e9e
svavil <sergei.vavilov@msdecisions.ru>
2020-08-20 00:13:21 +0300

    Logging numbers list separately for each chat

--- TG_bot_backbone.py
+++ TG_bot_backbone.py
@@ -14,8 +14,7 @@ def get_updates_json(request):
     response = requests.get(request + 'getUpdates')
     return response.json()
 
-def get_numbered_update(request, message_id):
-    json = get_updates_json(request)
+def get_numbered_update(json, message_id):
     if (message_id >= len(json['result'])):
         return np.NaN
     message_text = json['result'][message_id]['message']['text']
@@ -23,22 +22,6 @@ def get_numbered_update(request, message_id):
         return np.NaN
     return float(message_text)
 
-if '__main__' == __name__:
-    TOKEN = os.environ["BOT_TOKEN"]
-    url = "https://api.telegram.org/bot" + TOKEN + "/"
-    json = get_updates_json(url)
-    next_message_number = len(json['result'])
-    while(True):
-        number = get_numbered_update(url, next_message_number)
-        if ~np.isnan(number):
-            json = get_updates_json(url)
-            reply_text = 'Accepted'
-            params = {'chat_id': json['result'][next_message_number]['message']['chat']['id'], 'text': reply_text}
-            response = requests.post(url + 'sendMessage', data = params)
-            print(response)
-            next_message_number += 1
-        sleep(1)
-    
 def describe(number_list):
     import numpy as np
     count = len(number_list)
@@ -48,16 +31,24 @@ def describe(number_list):
     std = np.std(number_list)
     return "A list of " + str(count) + " samples with mean " + str(round(mean, 2)) + " and deviation " + str(round(std, 2))
 
-def describe_no_dependencies(number_list):
-    count = len(number_list)
-    if (0 == count):
-        return("No numbers in list")
-    sum = 0
-    for i in range(0, len(number_list)):
-        sum = sum + number_list[i]
-    mean = sum / count
-    sum_sq = 0
-    for i in range(0, len(number_list)):
-        sum_sq = sum_sq + (number_list[i] - mean)**2
-    std = (sum_sq / (count - 1))**0.5
-    return "A list of " + str(count) + " samples with mean " + str(round(mean, 2)) + " and deviation " + str(round(std, 2))
+if '__main__' == __name__:
+    TOKEN = os.environ["BOT_TOKEN"]
+    url = "https://api.telegram.org/bot" + TOKEN + "/"
+    json = get_updates_json(url)
+    next_message_number = len(json['result'])
+    statistics = {}
+    while(True):
+        json = get_updates_json(url)
+        number = get_numbered_update(json, next_message_number)
+        if ~np.isnan(number):
+            chat_id = json["result"][next_message_number]["message"]["chat"]["id"]
+            if chat_id in statistics.keys():
+                statistics[chat_id] = statistics[chat_id] + [number]
+            else:
+                statistics[chat_id] = [number]
+            reply_text = describe(statistics[chat_id])
+            params = {'chat_id': chat_id, 'text': reply_text}
+            response = requests.post(url + 'sendMessage', data = params)
+            print(response)
+            next_message_number += 1
+        sleep(1)
