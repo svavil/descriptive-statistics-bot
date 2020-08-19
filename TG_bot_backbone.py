aaff6fea73b6a8005c041c23c6385ce4e2dc904e
svavil <sergei.vavilov@msdecisions.ru>
2020-08-20 00:36:41 +0300

    Added support for /clear command

--- TG_bot_backbone.py
+++ TG_bot_backbone.py
@@ -6,9 +6,13 @@ This is a temporary script file.
 """
 
 import requests
-import numpy as np
 from time import sleep
 import os
+
+CODE_PASS = 0
+CODE_START = 1
+CODE_CLEAR = 2
+CODE_MESSAGE = 3
 
 def get_updates_json(request):  
     response = requests.get(request + 'getUpdates')
@@ -16,11 +20,14 @@ def get_updates_json(request):
 
 def get_numbered_update(json, message_id):
     if (message_id >= len(json['result'])):
-        return np.NaN
+        return -1, CODE_PASS, -1
+    chat_id = json["result"][message_id]["message"]["chat"]["id"]
     message_text = json['result'][message_id]['message']['text']
     if message_text == '/start':
-        return np.NaN
-    return float(message_text)
+        return chat_id, CODE_START, 0
+    if message_text == "/clear":
+        return chat_id, CODE_CLEAR, 0
+    return chat_id, CODE_MESSAGE, float(message_text)
 
 def describe(number_list):
     import numpy as np
@@ -39,16 +46,23 @@ if '__main__' == __name__:
     statistics = {}
     while(True):
         json = get_updates_json(url)
-        number = get_numbered_update(json, next_message_number)
-        if ~np.isnan(number):
-            chat_id = json["result"][next_message_number]["message"]["chat"]["id"]
+        chat_id, message_code, message_content = get_numbered_update(json, next_message_number)
+        if message_code == CODE_PASS:
+            sleep(1)
+            continue
+        if message_code == CODE_MESSAGE:
             if chat_id in statistics.keys():
-                statistics[chat_id] = statistics[chat_id] + [number]
+                statistics[chat_id] = statistics[chat_id] + [message_content]
             else:
-                statistics[chat_id] = [number]
+                statistics[chat_id] = [message_content]
             reply_text = describe(statistics[chat_id])
-            params = {'chat_id': chat_id, 'text': reply_text}
-            response = requests.post(url + 'sendMessage', data = params)
-            print(response)
-            next_message_number += 1
-        sleep(1)
+        if message_code == CODE_CLEAR:
+            statistics[chat_id] = []
+            reply_text = "Array cleared"
+        if message_code == CODE_START:
+            reply_text = "Enter one number at a time to accumulate statistics"
+        params = {'chat_id': chat_id, 'text': reply_text}
+        print(f"Sending message: {reply_text} to chat_id: {chat_id}")
+        response = requests.post(url + 'sendMessage', data = params)
+        print(response)
+        next_message_number += 1
